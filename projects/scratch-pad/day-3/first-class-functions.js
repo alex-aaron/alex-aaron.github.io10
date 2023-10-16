ffe6b97912bf7b9e74233de0f71b6b1ffa61f834
Alex Aaron <alexfrancisaaron@gmail.com>
2023-10-16 20:07:21 +0000

    matchy added

---
+++ projects/scratch-pad/day-3/first-class-functions.js
@@ -0,0 +1,125 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
+ */
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is greater than the 
+ * base.
+ */
+function createGreaterThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+
+    return function(secondValue) {
+        // if (base < secondValue) {
+        //     return true;
+        // } else if (base === secondValue) {
+        //     return 'equal';
+        // } else {
+        //     return false;
+        // }
+        return base < secondValue;
+    }
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input base to test against, which could be a String or Number, 
+ * return a Function that tests whether a given value is less than the 
+ * base.
+ */
+function createLessThanFilter(base) {
+    // YOUR CODE BELOW HERE //
+    
+    return function(){
+        
+    }
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a startsWith character, which will be a single character, return a 
+ * Function that tests whether a given String starts with the startsWith 
+ * character.
+ */
+function createStartsWithFilter(startsWith) {
+    // YOUR CODE BELOW HERE //
+    
+    return function(str){
+        return str[0].toUpperCase() === startsWith.toUpperCase();
+    }
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given a endsWith character, which will be a single character, return a 
+ * Function that tests whether a given String ends with the endsWith 
+ * character.
+ */
+function createEndsWithFilter(endsWith) {
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to modify a String, 
+ * return the Array of the Strings, modified.
+ * 
+ * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ * the modify Function, but we need to collect the results into some collection.
+ * 
+ * let uppercase = modifyStrings(['a', 'b', 'c'], function(string){ return string.toUpperCase() }); // ['A', 'B', 'C']
+ */
+
+
+function modifyStrings(strings, modify) { // modify is a FUNCTION
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an Array of Strings and a Function designed to test the String in some 
+ * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
+ * 
+ * Imagine you had a list of names, and you wanted to test they all 
+ * begin with "C", or they are all exclaimations that end with "!".
+ * 
+ * TIP: You need to loop over the Strings, right? And pass them to the test?
+ */
+function allStringsPass(strings, test) { // test is a FUNCTION that will return true or false
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.createGreaterThanFilter = createGreaterThanFilter;
+    module.exports.createLessThanFilter = createLessThanFilter;
+    module.exports.createStartsWithFilter = createStartsWithFilter;
+    module.exports.createEndsWithFilter = createEndsWithFilter;
+    module.exports.modifyStrings = modifyStrings;
+    module.exports.allStringsPass = allStringsPass;   
+}
---
+++ projects/scratch-pad/day-3/type.js
@@ -0,0 +1,104 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * IN CLASS EXERCISE: TYPE
+ */
+
+/** 
+ * Given an input value, return true if the value is an Array, false if otherwise.
+ * 
+ * TIP: In JavaScript, how can we decipher if a value is an Array? Can typeof
+ * work?
+ * 
+ * HINT: A method already exists that can be very helpful for this. You may need to google.
+ */
+function isArray(value) {
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input value, return true if the value is an Object intended as a 
+ * collection, false if otherwise.
+ * 
+ * TIP: In JavaScript, how can we decipher if a value is an Object, but not 
+ * null, not an Array, not a Date - all of these will return 'object' if used 
+ * with typeof.
+ * 
+ * // Look up how to figure out if something is an "instance" of the date object
+ * 
+ * isObject({ a: 1, b: 2}); // true
+ * isObject(null); // false
+ * isObject(new Date()); // false
+ */
+function isObject(value) {
+    // YOUR CODE BELOW HERE //
+    
+    // determine if the input value is considered an object but it is NOT null && not date object && not array
+    if (typeof value === 'object' && Array.isArray(value) === false && value !== null && value instanceof Date === false){
+        return true;
+    } else {
+        return false;
+    }
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/** 
+ * Given an input value, return true if is either an Array or an an Object 
+ * intended as a collection, false if otherwise.
+ * 
+ * TIP: Similar to isObject, but we must return true if the value is an Array.
+ */
+function isCollection(value) {
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+/**
+ * Given an input value, return the type of the value as a String
+ * 
+ * Types are one of: 
+ *    - "string"
+ *    - "array"
+ *    - "object"
+ *    - "undefined"
+ *    - "number"
+ *    - "boolean"
+ *    - "null"
+ *    - "function"
+ *    - "date"
+ * 
+ * Examples:
+ *    typeOf(134) -> "number"
+ *    typeOf("javascript") -> "string"
+ *    typeOf([1,2,3]) -> "array"
+ */ 
+function typeOf(value) {
+    // YOUR CODE BELOW HERE //
+    
+    
+    
+    
+    // YOUR CODE ABOVE HERE //
+}
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+    (typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.isArray = isArray;
+    module.exports.isObject = isObject;
+    module.exports.isCollection = isCollection;
+    module.exports.typeOf = typeOf;
+}
